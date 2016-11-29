366a99a6a1583828d76d1a68d07374b002cfbf14
unknown <ronald.junior@NAVE035-Q08H6P9.nave.local>
2016-11-29 15:00:09 -0200

    FirstCommitOfTheProject

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour 
+{
+    [SerializeField]
+    float speed;
+    Rigidbody2D rb;
+    Vector3 mousePosition;
+    Vector3 point;
+    void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+	void Update () 
+	{
+        mousePosition = Input.mousePosition;
+		if (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.D)) 
+		{
+            this.transform.position += (Vector3.up + Vector3.right) * speed * Time.deltaTime;
+		}
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += (Vector3.up + Vector3.left) * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += (Vector3.down + Vector3.right) * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += (Vector3.down + Vector3.left) * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        
+	}
+}
