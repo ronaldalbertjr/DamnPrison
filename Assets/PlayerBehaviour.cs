7d2b557ead1ff1b9b976fb36cd901cdcd1bd3069
ronaldalbertjr <ronaldalbert1609@gmail.com>
2016-11-30 07:56:11 -0200

    AddedTheHorizontalAndVerticalAnimationsWalkingForThePlayer
    Succeded in animating with unity

--- Assets/PlayerBehaviour.cs
+++ Assets/PlayerBehaviour.cs
@@ -6,11 +6,13 @@ public class PlayerBehaviour : MonoBehaviour
     [SerializeField]
     float speed;
     Rigidbody2D rb;
+	Animator anim;
     Vector3 mousePosition;
     Vector3 point;
     void Awake()
     {
         rb = this.GetComponent<Rigidbody2D>();
+		anim = this.GetComponent<Animator> ();
     }
 	void Update () 
 	{
@@ -34,18 +36,34 @@ public class PlayerBehaviour : MonoBehaviour
         else if (Input.GetKey(KeyCode.W))
         {
             this.transform.position += Vector3.up * speed * Time.deltaTime;
+			anim.SetBool("WalkingUp", true);
+			anim.SetBool("WalkingLeft", false);
+			anim.SetBool("WalkingDown", false);
+			anim.SetBool("WalkingRight", false);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             this.transform.position += Vector3.right * speed * Time.deltaTime;
+			anim.SetBool("WalkingLeft", true);
+			anim.SetBool("WalkingUp", false);
+			anim.SetBool("WalkingRight", false);
+			anim.SetBool("WalkingDown", true);
         }
         else if (Input.GetKey(KeyCode.A))
         {
             this.transform.position += Vector3.left * speed * Time.deltaTime;
+			anim.SetBool("WalkingRight", true);
+			anim.SetBool("WalkingUp", false);
+			anim.SetBool("WalkingLeft", false);
+			anim.SetBool("WalkingDown", false);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             this.transform.position += Vector3.down * speed * Time.deltaTime;
+			anim.SetBool("WalkingDown", true);
+			anim.SetBool("WalkingUp", false);
+			anim.SetBool("WalkingLeft", false);
+			anim.SetBool("WalkingRight", false);
         }
         
 	}
