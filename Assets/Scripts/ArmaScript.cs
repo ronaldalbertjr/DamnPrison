9cfaaf8eec526750577dfac929319ffd438c2b67
Ronald Albert <ronaldalbert1609@gmail.com>
2016-12-02 09:36:45 -0200

    FinishedAllTheAnimations
    FinallyFinishedAllTheAnimationsForTheMainCharacter

---
+++ Assets/Scripts/ArmaScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmaScript : MonoBehaviour
+{
+    Vector3 mousePosition;
+    Vector3 lookPosition;
+    Quaternion rotation;
+	void Update ()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lookPosition = mousePosition - this.transform.position;
+        rotation = Quaternion.LookRotation(lookPosition);
+        rotation = new Quaternion(0f, 0f, rotation.z / 2, rotation.w / 2);
+        this.transform.rotation = rotation;
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour 
+{
+    [SerializeField]
+    float speed;
+    Rigidbody2D rb;
+	Animator anim;
+    void Awake()
+    {
+		anim = this.GetComponent<Animator> ();
+    }
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.D)) 
+		{
+            anim.SetFloat("WalkingFloat", 4);
+            this.transform.position += (Vector3.up + Vector3.right) * speed * Time.deltaTime;
+		}
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += (Vector3.up + Vector3.left) * speed * Time.deltaTime;
+            anim.SetFloat("WalkingFloat", 5);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += (Vector3.down + Vector3.right) * speed * Time.deltaTime;
+            anim.SetFloat("WalkingFloat", 6);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += (Vector3.down + Vector3.left) * speed * Time.deltaTime;
+            anim.SetFloat("WalkingFloat", 7);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += Vector3.up * speed * Time.deltaTime;
+            anim.SetFloat("WalkingFloat", 2);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position += Vector3.right * speed * Time.deltaTime;
+            anim.SetFloat("WalkingFloat", 3);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += Vector3.left * speed * Time.deltaTime;
+            anim.SetFloat("WalkingFloat", 1);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetFloat("WalkingFloat", 0);
+            this.transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        
+	}
+}
