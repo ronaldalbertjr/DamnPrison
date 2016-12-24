2b50d61310bfb5cfe7fb6f13eb99abd0863c9817
Ronald Albert <ronaldalbert1609@gmail.com>
2016-12-24 13:59:02 -0200

    PlayerAnimationFinished
    Made a different animator controller for both body and legs of the player

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,16 +7,20 @@ public class PlayerBehaviour : MonoBehaviour
     float speed;
     [SerializeField]
     GameObject aux;
-
-
-    Rigidbody2D rb;
-	Animator anim;
+    [SerializeField]
+    GameObject body;
+    [SerializeField]
+    GameObject legs;
+    
+	Animator bodyAnim;
+    Animator legsAnim;
     float angle;
 
 
     void Awake()
     {
-		anim = this.GetComponent<Animator> ();
+		bodyAnim = body.GetComponent<Animator> ();
+        legsAnim = legs.GetComponent<Animator> ();
     }
 	void Update () 
 	{
@@ -29,34 +33,42 @@ public class PlayerBehaviour : MonoBehaviour
         if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
         {
             this.transform.position += (Vector3.up + Vector3.right) * speed * Time.deltaTime;
+            legsAnim.SetFloat("WalkingLeg", 2);
         }
         else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
         {
             this.transform.position += (Vector3.up + Vector3.left) * speed * Time.deltaTime;
+            legsAnim.SetFloat("WalkingLeg", 2);
         }
         else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
         {
             this.transform.position += (Vector3.down + Vector3.right) * speed * Time.deltaTime;
+            legsAnim.SetFloat("WalkingLeg", 0);
         }
         else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
         {
             this.transform.position += (Vector3.down + Vector3.left) * speed * Time.deltaTime;
+            legsAnim.SetFloat("WalkingLeg", 0);
         }
         else if (Input.GetKey(KeyCode.W))
         {
             this.transform.position += Vector3.up * speed * Time.deltaTime;
+            legsAnim.SetFloat("WalkingLeg", 2);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             this.transform.position += Vector3.right * speed * Time.deltaTime;
+            legsAnim.SetFloat("WalkingLeg", 1);
         }
         else if (Input.GetKey(KeyCode.A))
         {
             this.transform.position += Vector3.left * speed * Time.deltaTime;
+            legsAnim.SetFloat("WalkingLeg", 3);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             this.transform.position += Vector3.down * speed * Time.deltaTime;
+            legsAnim.SetFloat("WalkingLeg", 0);
         }
     }
     void ChangeRotation()
@@ -64,35 +76,35 @@ public class PlayerBehaviour : MonoBehaviour
         angle = aux.transform.eulerAngles.z;
         if (angle > 157.5 && angle < 202.5)
         {
-            anim.SetFloat("WalkingFloat", 0);
+            bodyAnim.SetFloat("WalkingBody", 0);
         }
         else if (angle > 247.5 && angle < 292.5)
         {
-            anim.SetFloat("WalkingFloat", 3);
+            bodyAnim.SetFloat("WalkingBody", 1);
         }
         else if (angle > 67.5 && angle < 112.5)
         {
-            anim.SetFloat("WalkingFloat", 1);
+            bodyAnim.SetFloat("WalkingBody", 3);
         }
         else if(angle >= 22.5 && angle <= 67.5)
         {
-            anim.SetFloat("WalkingFloat", 5);
+            bodyAnim.SetFloat("WalkingBody", 5);
         }
         else if(angle >= 292.5 && angle <= 337.5)
         {
-            anim.SetFloat("WalkingFloat", 4);
+            bodyAnim.SetFloat("WalkingBody", 4);
         }
         else if(angle >= 112.5 && angle <= 157.5)
         {
-            anim.SetFloat("WalkingFloat", 7);
+            bodyAnim.SetFloat("WalkingBody", 7);
         }
         else if(angle >= 202.5 && angle <= 247.5)
         {
-            anim.SetFloat("WalkingFloat", 6);
+            bodyAnim.SetFloat("WalkingBody", 6);
         }
         else if (angle < 22.5 || angle > 337.5)
         {
-            anim.SetFloat("WalkingFloat", 2);
+            bodyAnim.SetFloat("WalkingBody", 2);
         }
     }
 }
