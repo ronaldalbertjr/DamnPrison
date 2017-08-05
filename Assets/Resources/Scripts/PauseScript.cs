aa45e24f29e9bb805bb75daeed895ba4858fc134
ronaldalbertjr <ronaldalbert1609@gmail.com>
2017-08-05 00:25:50 -0300

    WorkingOnOptionsMenu

--- Assets/Resources/Scripts/PauseScript.cs
+++ Assets/Resources/Scripts/PauseScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseScript : MonoBehaviour
 {
@@ -10,10 +11,50 @@ public class PauseScript : MonoBehaviour
         GameObject menuPanel;
         [SerializeField]
         GameObject exitPanel;
+        [SerializeField]
+        Toggle toggle;
+        [SerializeField]
+        Dropdown dropdown;
     #endregion
 
     void Start()
     {
+        if(Screen.fullScreen)
+        {
+            toggle.isOn = false;
+        }
+        else
+        {
+            toggle.isOn = true;
+        }
+        if(Screen.width == 1920 && Screen.height == 1080)
+        {
+            dropdown.value = 0;
+        }
+        else if(Screen.width == 1680 && Screen.height == 1050)
+        {
+            dropdown.value = 1;
+        }
+        else if (Screen.width == 1600 && Screen.height == 900)
+        {
+            dropdown.value = 2;
+        }
+        else if (Screen.width == 1440 && Screen.height == 900)
+        {
+            dropdown.value = 3;
+        }
+        else if (Screen.width == 1366 && Screen.height == 768)
+        {
+            dropdown.value = 4;
+        }
+        else if (Screen.width == 1280 && Screen.height == 1024)
+        {
+            dropdown.value = 5;
+        }
+        else if (Screen.width == 1024 && Screen.height == 768)
+        {
+            dropdown.value = 6;
+        }
         GetComponent<Canvas>().enabled = false;
     }
 
@@ -54,4 +95,38 @@ public class PauseScript : MonoBehaviour
         menuPanel.SetActive(true);
         exitPanel.SetActive(false);
     }
+
+    public void OnToggleValueChanged(Toggle toggle)
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+    
+    public void OnDropdownValueChanged(Dropdown dropdown)
+    {
+        switch(dropdown.value)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                break;
+            case 1:
+                Screen.SetResolution(1680, 1050, Screen.fullScreen);
+                break;
+            case 2:
+                Screen.SetResolution(1600, 900, Screen.fullScreen);
+                break;
+            case 3:
+                Screen.SetResolution(1440, 900, Screen.fullScreen);
+                break;
+            case 4:
+                Screen.SetResolution(1366, 768, Screen.fullScreen);
+                break;
+            case 5:
+                Screen.SetResolution(1280, 1024, Screen.fullScreen);
+                break;
+            case 6:
+                Screen.SetResolution(1024, 768, Screen.fullScreen);
+                break;
+        }
+    }
+    
 }
