e3113c8422f2b578f974693a4933bbfd8e7731ee
ronaldalbertjr <ronaldalbert1609@gmail.com>
2017-08-21 14:48:21 -0300

    AddedInitialCutscene

---
+++ Assets/Resources/Scripts/CutsceneScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CutsceneScript : MonoBehaviour
+{
+    #region Variables
+        [SerializeField]
+        MovieTexture movie;
+    #endregion
+
+    void Start ()
+    {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        GetComponent<AudioSource>().clip = movie.audioClip;
+
+        movie.Play();
+        Invoke("RunGameScene", 36f);		
+	}
+
+    private void Update()
+    {
+        if(Input.anyKey)
+        {
+            RunGameScene();
+        }
+    }
+
+    void RunGameScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+	
+}
